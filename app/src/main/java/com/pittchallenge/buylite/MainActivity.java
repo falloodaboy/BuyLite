ba02961561d60156d6bdbdd0f3e8401de8e0f798
falloodaboy <40337476+falloodaboy@users.noreply.github.com>
2019-08-04 19:08:01 -0400

    App Options Added

--- app/src/main/java/com/pittchallenge/buylite/MainActivity.java
+++ app/src/main/java/com/pittchallenge/buylite/MainActivity.java
@@ -1,14 +1,17 @@
 package com.pittchallenge.buylite;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private LifeCycleObserver observer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        observer = new LifeCycleObserver();
+        this.getLifecycle().addObserver(observer);
     }
+
+
 }
