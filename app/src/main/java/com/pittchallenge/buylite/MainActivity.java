a3dc624c54b4ac3da6134968127499bc03f65446
falloodaboy <40337476+falloodaboy@users.noreply.github.com>
2019-08-05 18:44:45 -0400

    SignUpActivity added with layout.

--- app/src/main/java/com/pittchallenge/buylite/MainActivity.java
+++ app/src/main/java/com/pittchallenge/buylite/MainActivity.java
@@ -1,10 +1,14 @@
 package com.pittchallenge.buylite;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private LifeCycleObserver observer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,5 +17,9 @@ public class MainActivity extends AppCompatActivity {
         this.getLifecycle().addObserver(observer);
     }
 
+    public void launchSignUp(View view){
+        Intent intent = new Intent(this, SignUpActivity.class);
+        this.startActivity(intent);
+    }
 
 }
---
+++ app/src/main/java/com/pittchallenge/buylite/SignUpActivity.java
@@ -0,0 +1,26 @@
+package com.pittchallenge.buylite;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class SignUpActivity extends AppCompatActivity {
+      //  ViewModel model;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        //model = ViewModelProviders.of(this).
+    }
+
+    private void SignUp(View view){
+        /*
+        * Use ViewModel to sign the User Up
+        * after checking a few properties
+        * of the information entered
+        * */
+    }
+}
